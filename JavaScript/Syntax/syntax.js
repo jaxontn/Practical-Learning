f9a3997b25934b25b1bf254ec5bbb65c79c15f5c
jaxontn <jaxontn.tech@gmail.com>
2022-06-29 12:43:32 +0800

    updated

---
+++ JavaScript/Syntax/syntax.js
@@ -0,0 +1,16 @@
+// single-line comment
+/*
+Comment with multiple lines
+*/
+
+
+//assertions
+//assert.equal(7 + 1, 8)
+//it means 7 + 1 = 8
+
+// Printing a value to standard out (another method call)
+// console.log('Hello');
+
+// Printing error information to standard error
+// console.error('Something went wrong!');
+
